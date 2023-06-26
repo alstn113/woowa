0d4fa3a806d656727e8b47adfbe23726999764fd
김민수 MINSOO KIM <alstn113@gmail.com>
2023-06-26 14:17:34 +0900

    feat: onboarding-problem5

--- onboarding/src/problem5.js
+++ onboarding/src/problem5.js
@@ -1,6 +1,11 @@
-function problem5(money) {
-  var answer;
+const problem5 = (money) => {
+  answer = [];
+  for (m of [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1]) {
+    answer.push(parseInt(money / m));
+    money = money % m;
+  }
+
   return answer;
-}
+};
 
 module.exports = problem5;
