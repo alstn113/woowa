f444fbcf4b91743c17604c8ab2ba1570c957eb6a
김민수 MINSOO KIM <alstn113@gmail.com>
2023-06-26 15:13:43 +0900

    feat: onboarding-problem6

--- onboarding/src/problem6.js
+++ onboarding/src/problem6.js
@@ -1,6 +1,25 @@
-function problem6(forms) {
-  var answer;
-  return answer;
-}
+const problem6 = (forms) => {
+  const set = new Set();
+  const dic = {};
+
+  forms.forEach((form) => {
+    const [email, nickname] = form;
+    if (!email.endsWith("@email.com")) return;
+    nickname
+      .split("")
+      .filter((_, idx) => idx !== nickname.length - 1)
+      .forEach((_, idx) => {
+        const word = nickname[idx] + nickname[idx + 1];
+        if (dic[word]) {
+          set.add(email);
+          set.add(dic[word]);
+        } else {
+          dic[word] = email;
+        }
+      });
+  });
+
+  return [...set].sort();
+};
 
 module.exports = problem6;
