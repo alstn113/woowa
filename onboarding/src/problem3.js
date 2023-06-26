bbcb590290fa773f09d57f27bb28681192f7e811
김민수 MINSOO KIM <alstn113@gmail.com>
2023-06-26 13:37:13 +0900

    feat: onboarding-problem3

--- onboarding/src/problem3.js
+++ onboarding/src/problem3.js
@@ -1,6 +1,17 @@
-function problem3(number) {
-  var answer;
+const problem3 = (number) => {
+  let answer = 0;
+  for (let i = 1; i <= number; i++) {
+    v = String(i).split("");
+    tmp = 0;
+    for (let j = 0; j < v.length; j++) {
+      if (v[j] === "3" || v[j] === "6" || v[j] === "9") {
+        tmp += 1;
+      }
+    }
+    answer += tmp;
+  }
+
   return answer;
-}
+};
 
 module.exports = problem3;
