69245d2a6313b07dbaad7937a398034e759df8e7
김민수 MINSOO KIM <alstn113@gmail.com>
2023-06-26 13:30:44 +0900

    feat: onboarding-problem2

--- onboarding/src/problem2.js
+++ onboarding/src/problem2.js
@@ -1,6 +1,16 @@
-function problem2(cryptogram) {
-  var answer;
-  return answer;
-}
+const problem2 = (cryptogram) => {
+  arr = cryptogram.split("");
+  stack = [];
+  while (arr.length > 0) {
+    v = arr.shift();
+    if (stack[stack.length - 1] === v) {
+      stack.pop();
+    } else {
+      stack.push(v);
+    }
+  }
+
+  return stack.join("");
+};
 
 module.exports = problem2;
