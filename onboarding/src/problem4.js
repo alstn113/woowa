8926dc101135b9bb6dc4c18e4d6e9951884f435b
김민수 MINSOO KIM <alstn113@gmail.com>
2023-06-26 14:07:54 +0900

    feat: onboarding-problem4

--- onboarding/src/problem4.js
+++ onboarding/src/problem4.js
@@ -1,6 +1,21 @@
-function problem4(word) {
-  var answer;
-  return answer;
-}
+const problem4 = (word) => {
+  const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lower = "abcdefghijklmnopqrstuvwxyz";
+
+  return word
+    .split("")
+    .map((v) => {
+      if (upper.includes(v)) {
+        return upper[26 - upper.indexOf(v) - 1];
+      } else if (lower.includes(v)) {
+        return lower[26 - lower.indexOf(v) - 1];
+      } else {
+        return v;
+      }
+    })
+    .join("");
+};
+
+console.log(problem4("I love you"));
 
 module.exports = problem4;
