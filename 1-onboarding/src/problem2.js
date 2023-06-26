91407bd2005968477c5ded3599803d9782d605be
김민수 MINSOO KIM <alstn113@gmail.com>
2023-06-26 20:40:48 +0900

    refactor: onboarding-problem2

--- 1-onboarding/src/problem2.js
+++ 1-onboarding/src/problem2.js
@@ -1,16 +1,26 @@
-const problem2 = (cryptogram) => {
-  arr = cryptogram.split("");
-  stack = [];
+/**
+ * 스택을 사용하여 중복된 문자를 제거하는 함수
+ * @param {string} arr - 중복 문자를 제거할 배열
+ * @returns {string} - 중복 문자가 제거된 문자열
+ */
+const removeDuplicates = (arr) => {
+  const stack = [];
   while (arr.length > 0) {
-    v = arr.shift();
-    if (stack[stack.length - 1] === v) {
-      stack.pop();
-    } else {
-      stack.push(v);
-    }
+    const v = arr.shift();
+    if (stack[stack.length - 1] === v) stack.pop();
+    else stack.push(v);
   }
-
   return stack.join("");
+};
+
+/**
+ * 주어진 문자열에서 중복 문자를 제거하는 함수
+ * @param {string} cryptogram - 중복 문자를 제거할 문자열
+ * @returns {string} - 중복 문자가 제거된 결과 문자열
+ */
+const problem2 = (cryptogram) => {
+  const arr = cryptogram.split("");
+  return removeDuplicates(arr);
 };
 
 module.exports = problem2;
