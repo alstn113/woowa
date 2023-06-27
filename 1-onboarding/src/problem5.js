405c1179563216a38f1c66e554871b1a8d2d577a
김민수 MINSOO KIM <alstn113@gmail.com>
2023-06-27 10:23:35 +0900

    refactor: onboarding-problem5

--- 1-onboarding/src/problem5.js
+++ 1-onboarding/src/problem5.js
@@ -1,12 +1,19 @@
 /**
+ * 지폐와 동전의 단위 리스트
+ * @type {number[]}
+ */
+const billList = Object.freeze([
+  50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1,
+]);
+
+/**
  * 주어진 금액을 지폐와 동전 단위로 나누어 개수를 반환하는 함수
  * @param {number} money - 금액
  * @returns {number[]} - 지폐와 동전 단위별 개수 배열
  */
 const problem5 = (money) => {
-  const billLish = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   answer = [];
-  for (bill of billLish) {
+  for (bill of billList) {
     answer.push(parseInt(money / bill));
     money = money % bill;
   }
