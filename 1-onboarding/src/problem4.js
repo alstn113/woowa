8168c467f789fb40ff1cd9c4b2bcc2330e0d5cb3
김민수 MINSOO KIM <alstn113@gmail.com>
2023-06-27 10:11:18 +0900

    refactor: onboarding-problem4

--- 1-onboarding/src/problem4.js
+++ 1-onboarding/src/problem4.js
@@ -1,21 +1,50 @@
-const problem4 = (word) => {
+/**
+ * 대문자 알파벳을 뒤집는 함수
+ * @param {string} char - 대문자 알파벳
+ * @returns {string} - 뒤집힌 대문자 알파벳
+ */
+const reverseUpperCase = (char) => {
   const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const lower = "abcdefghijklmnopqrstuvwxyz";
+  return upper[26 - upper.indexOf(char) - 1];
+};
 
+/**
+ * 소문자 알파벳을 뒤집는 함수
+ * @param {string} char - 소문자 알파벳
+ * @returns {string} - 뒤집힌 소문자 알파벳
+ */
+const reverseLowerCase = (char) => {
+  const lower = "abcdefghijklmnopqrstuvwxyz";
+  return lower[26 - lower.indexOf(char) - 1];
+};
+
+/**
+ * 주어진 단어에서 알파벳을 뒤집는 함수
+ * @param {string} word - 뒤집어야 할 단어
+ * @returns {string} - 알파벳이 뒤집힌 결과 단어
+ */
+const reverseWord = (word) => {
   return word
     .split("")
-    .map((v) => {
-      if (upper.includes(v)) {
-        return upper[26 - upper.indexOf(v) - 1];
-      } else if (lower.includes(v)) {
-        return lower[26 - lower.indexOf(v) - 1];
+    .map((char) => {
+      if (char >= "A" && char <= "Z") {
+        return reverseUpperCase(char);
+      } else if (char >= "a" && char <= "z") {
+        return reverseLowerCase(char);
       } else {
-        return v;
+        return char;
       }
     })
     .join("");
 };
 
-console.log(problem4("I love you"));
+/**
+ * 주어진 단어의 알파벳을 뒤집는 함수
+ * @param {string} word - 뒤집어야 할 단어
+ * @returns {string} - 알파벳이 뒤집힌 결과 단어
+ */
+const problem4 = (word) => {
+  return reverseWord(word);
+};
 
 module.exports = problem4;
