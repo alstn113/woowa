39f5d7940044ad741b275c59c7cf91c5af0ac6d2
김민수 MINSOO KIM <alstn113@gmail.com>
2023-06-28 16:37:14 +0900

    feat: bridge constant messages

--- 4-bridge/src/constants.js
+++ 4-bridge/src/constants.js
@@ -1,3 +1,21 @@
-const MESSAGES = Object.freeze({});
+const MESSAGES = Object.freeze({
+  START_GAME: '다리 건너기 게임을 시작합니다.',
+  ENTER_BRIDGE_LENGTH: '다리의 길이를 입력해주세요.',
+  ENTER_MOVING: '이동할 칸을 선택해주세요. (위: U, 아래: D)',
+  SELECT_RETRY_OR_EXIT:
+    '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)',
+  BRIDGE_RESULT: '최종 게임 결과',
+  GAME_RESULT: '게임 성공 여부',
+  SUCCESS: '성공',
+  FAIL: '실패',
+  TOTAL_ATTEMPTS: '총 시도한 횟수',
+});
 
 const ERROR = Object.freeze({});
+
+const COMMAND = Object.freeze({
+  RETRY: 'R',
+  EXIT: 'Q',
+  UP: 'U',
+  DOWN: 'D',
+});
--- 4-bridge/src/views/OutputView.js
+++ 4-bridge/src/views/OutputView.js
@@ -1,7 +1,15 @@
+const { Console } = require('@woowacourse/mission-utils');
+const { MESSAGES } = require('../constants');
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 const OutputView = {
+
+  startGame() {
+    Console.print(MESSAGES.START_GAME);
+  }
+
   /**
    * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
    * <p>
