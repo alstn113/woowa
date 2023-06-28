fde1c61b16feb358927c91a2ebe03d59d6911042
김민수 MINSOO KIM <alstn113@gmail.com>
2023-06-28 15:01:20 +0900

    feat: lotto number test

--- 3-lotto/__tests__/LottoTest.js
+++ 3-lotto/__tests__/LottoTest.js
@@ -16,4 +16,27 @@ describe('로또 클래스 테스트', () => {
   });
 
   // 아래에 추가 테스트 작성 가능
+  test('로또 번호가 정상인 경우이다.', () => {
+    const numbers = [1, 2, 3, 4, 5, 6];
+    expect(new Lotto(numbers).numbers).toEqual(numbers);
+  });
+
+  test('로또 번호 범위가 벗어나면 예외가 발생한다. (lotto <= 45)', () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 46]);
+    }).toThrow(`[ERROR] ${ERROR_MESSAGES.LOTTO_OUT_OF_RANGE}`);
+  });
+
+  test('로또 번호 범위가 벗어나면 예외가 발생한다. (lotto > 0)', () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, -1]);
+    }).toThrow(`[ERROR] ${ERROR_MESSAGES.LOTTO_OUT_OF_RANGE}`);
+  });
+
+  test('로또 번호의 타입이 잘못된 경우 예외가 발생한다.', () => {
+    const numbers = [1, 2, 3, 4, 5, '6'];
+    expect(() => {
+      new Lotto(numbers);
+    }).toThrow(`[ERROR] ${ERROR_MESSAGES.LOTTO_WRONG_TYPE}`);
+  });
 });
