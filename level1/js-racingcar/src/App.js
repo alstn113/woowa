3724e9893f08232a0b5387cb57cc5dd3fb811273
김민수 MINSOO KIM <alstn113@gmail.com>
2023-07-01 12:53:41 +0900

    feat: inputView async input

--- level1/js-racingcar/src/App.js
+++ level1/js-racingcar/src/App.js
@@ -1,8 +1,25 @@
+import { Console } from './utils/Console';
 class App {
   constructor() {}
 
-  play() {
-    return;
+  async processInput() {
+    try {
+      const input = await Console.readLine();
+      console.log(`You entered: ${input}`);
+    } catch (error) {
+      console.error('Error occurred:', error);
+    } finally {
+      Console.close();
+    }
+  }
+
+  async play() {
+    Console.print(
+      '경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)',
+    );
+    await this.processInput();
+
+    Console.print('시도할 회수는 몇회인가요?');
   }
 }
 
--- level1/js-racingcar/src/views/InputView.js
+++ level1/js-racingcar/src/views/InputView.js
@@ -1,3 +1,15 @@
-class InputView {}
+import Console from '../utils/Console.js';
+
+class InputView {
+  async readCarNames() {
+    const input = await Console.readLine();
+    return input;
+  }
+
+  async readTryCount() {
+    const input = await Console.readLine();
+    return input;
+  }
+}
 
 export default InputView;
