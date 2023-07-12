b7eb4d6065451d906533f4bf8cd522f1001b95de
김민수 MINSOO KIM <alstn113@gmail.com>
2023-07-12 21:29:35 +0900

    feat: storybook preview set

--- level2/react-payments/.storybook/preview.tsx
+++ level2/react-payments/.storybook/preview.tsx
@@ -1,4 +1,6 @@
 import type { Preview } from '@storybook/react';
+import AppProvider from '../src/AppProvider';
+import React from 'react';
 
 const preview: Preview = {
   parameters: {
@@ -10,6 +12,13 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [
+    (Story) => (
+      <AppProvider>
+        <Story />
+      </AppProvider>
+    ),
+  ],
 };
 
 export default preview;
