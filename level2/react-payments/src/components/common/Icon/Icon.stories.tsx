274a12ab5c6ae4d41fa46cd090e2bb3d4526fb21
김민수 MINSOO KIM <alstn113@gmail.com>
2023-07-16 21:16:18 +0900

    fix: storybook vite error를 vite version up으로 해결

---
+++ level2/react-payments/src/components/common/Icon/Icon.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Icon from './Icon';
+
+const meta = {
+  title: 'common/Icon',
+  component: Icon,
+} satisfies Meta<typeof Icon>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    src: 'https://avatars.githubusercontent.com/u/75781414?v=4',
+    alt: 'avatar',
+    w: 20,
+    h: 20,
+  },
+};
