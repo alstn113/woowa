79b7b71e70e38c343f150da36540fdf8318f6f40
김민수 MINSOO KIM <alstn113@gmail.com>
2023-07-18 03:32:20 +0900

    feat: dynamic route backup

--- level2/react-shopping-cart/src/api/test.tsx
+++ level2/react-shopping-cart/src/api/test.tsx
@@ -7,11 +7,15 @@ type PathParams<Path extends string> =
     ? PathParams<`:${Rest}`>
     : never;
 
-type Params = PathParams<':siteId/user/:userId'>;
+type Params1 = PathParams<'/site/:siteId/user/:userId'>;
+type Params2 = PathParams<'/dashboard'>;
 
 type PathArgs<Path extends string> = { [K in PathParams<Path>]: string };
 
-type Result = PathArgs<'/dashboard/:siteId/user:userId'>;
+type Result1 = PathArgs<'/dashboard/:siteId/user:userId'>;
+type Result2 = PathArgs<'/dashboard'>;
+
+const a: Result2 | null = null;
 
 type Props<Path extends string> = {
   path: Path;
@@ -29,7 +33,12 @@ const Test = () => {
         path="/dashboard/:siteId/user/:userId"
         params={{ siteId: '1', userId: '1' }}
       />
-      <Route path="/dashboard" params={{}} />
+      <Route
+        path="/dashboard"
+        params={{
+          a: 1,
+        }}
+      />
     </>
   );
 };
