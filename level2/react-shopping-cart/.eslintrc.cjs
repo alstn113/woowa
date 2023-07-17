22af3c9eee937f692fa4e1a9fef2e7920f11201d
김민수 MINSOO KIM <alstn113@gmail.com>
2023-07-17 18:23:53 +0900

    feat: 프로젝트 모듈 세팅

--- level2/react-shopping-cart/.eslintrc.cjs
+++ level2/react-shopping-cart/.eslintrc.cjs
@@ -2,12 +2,18 @@
 
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:react-hooks/recommended',
+    'plugin:storybook/recommended',
+    'plugin:import/recommended',
+    'plugin:prettier/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -18,10 +24,33 @@ module.exports = {
   },
   plugins: ['react-refresh'],
   rules: {
+    'react/react-in-jsx-scope': 'off',
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
+      {
+        allowConstantExport: true,
+      },
     ],
     '@typescript-eslint/no-non-null-assertion': 'off',
+    // storybook play에서 await을 사용할 수 없어서 off
+    '@typescript-eslint/await-thenable': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', ['parent', 'sibling'], 'index'],
+        pathGroups: [
+          {
+            pattern: 'angular',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        'newlines-between': 'always',
+      },
+    ],
   },
-}
+};
