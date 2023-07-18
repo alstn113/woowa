cf170ac16e12abd64a8a2acfe5c9dff4c5ef3bef
김민수 MINSOO KIM <alstn113@gmail.com>
2023-07-18 20:40:18 +0900

    feat: storybook setting

--- level2/react-shopping-cart/.storybook/preview.tsx
+++ level2/react-shopping-cart/.storybook/preview.tsx
@@ -1,4 +1,7 @@
 import type { Preview } from '@storybook/react';
+import React from 'react';
+import { RecoilRoot } from 'recoil';
+import GlobalStyle from '../src/styles/GlobalStyle';
 
 const preview: Preview = {
   parameters: {
@@ -10,6 +13,14 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [
+    (Story) => (
+      <RecoilRoot>
+        <GlobalStyle />
+        <Story />
+      </RecoilRoot>
+    ),
+  ],
 };
 
 export default preview;
--- level2/react-shopping-cart/src/pages/CartPage.tsx
+++ level2/react-shopping-cart/src/pages/CartPage.tsx
@@ -1,19 +1,4 @@
 const CartPage = () => {
-  const path = '/post/:postId/comment/:commentId';
-  const paramRegex = /:(\w+)/g;
-
-  const matches = path.match(paramRegex);
-  console.log(matches);
-  const params: { [key: string]: string } = {};
-  matches?.forEach((paramName) => {
-    params[paramName.slice(1)] = 'some-value';
-  });
-
-  if (matches) {
-    const paramName = matches[0].slice(1); // ":" 제외하고 파라미터 이름 추출
-    console.log(paramName); // 출력: "postId"
-  }
-
   return <div>CartPage</div>;
 };
 
