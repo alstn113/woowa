d4550795db61b7f29c460fe5873fc370988e7e92
김민수 MINSOO KIM <alstn113@gmail.com>
2023-07-24 21:46:00 +0900

    feat: base module

--- level2/react-shopping-cart-prod/.eslintrc.cjs
+++ level2/react-shopping-cart-prod/.eslintrc.cjs
@@ -3,16 +3,63 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    // prettier은 항상 마지막에 위치한다.
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    project: './tsconfig.json',
+  },
   plugins: ['react-refresh'],
   rules: {
+    // import React from 'react' 없이 사용 가능
+    'react/react-in-jsx-scope': 'off',
+    // 컴포넌트 arrow function
+    'react/function-component-definition': [
+      'warn',
+      { namedComponents: 'arrow-function' },
+    ],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    // import 순서 관련
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', ['parent', 'sibling'], 'index'],
+        pathGroups: [
+          {
+            pattern: 'react*',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react*'],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        'newlines-between': 'always',
+      },
+    ],
   },
-}
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
