92329b519fdaa7711c15bcf71ddb335424bf5c4b
김민수 MINSOO KIM <alstn113@gmail.com>
2023-07-25 17:50:32 +0900

    chore: remove serviceWorker url

--- level2/react-shopping-cart-prod/src/mocks/index.ts
+++ level2/react-shopping-cart-prod/src/mocks/index.ts
@@ -1,13 +1,9 @@
 import { setupWorker } from 'msw';
 
-import { BASE_URL } from '../constants';
 import { handlers } from './handlers';
 
 const worker = setupWorker(...handlers);
 
 worker.start({
   onUnhandledRequest: 'bypass',
-  serviceWorker: {
-    url: `${BASE_URL}/mockServiceWorker.js`,
-  },
 });
