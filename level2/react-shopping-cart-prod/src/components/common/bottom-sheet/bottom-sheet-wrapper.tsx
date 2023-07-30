762cf0ae0e1e90c4ea5e80d3ee25806f23f2a556
김민수 MINSOO KIM <alstn113@gmail.com>
2023-07-30 19:16:54 +0900

    feat: use bottom sheet으로 분리

--- level2/react-shopping-cart-prod/src/components/common/bottom-sheet/bottom-sheet-wrapper.tsx
+++ level2/react-shopping-cart-prod/src/components/common/bottom-sheet/bottom-sheet-wrapper.tsx
@@ -1,91 +1,14 @@
-import { useEffect, useRef } from 'react';
-
 import { motion } from 'framer-motion';
 import styled from '@emotion/styled';
 
-import { useBottomSheetContext } from './bottom-sheet-context';
+import useBottomSheet from './use-bottom-sheet';
 
 interface BottomSheetWrapperProps {
   children: React.ReactNode;
 }
 
 const BottomSheetWrapper = ({ children }: BottomSheetWrapperProps) => {
-  interface Metrics {
-    isDragging: boolean;
-    startY: number;
-    startHeight: number;
-  }
-
-  const metricsRef = useRef<Metrics>({
-    isDragging: false,
-    startY: 0,
-    startHeight: 0,
-  });
-
-  const sheetRef = useRef<HTMLDivElement>(null);
-
-  const { onClose } = useBottomSheetContext();
-
-  useEffect(() => {
-    const dragStart = (e: MouseEvent | TouchEvent) => {
-      console.log('dragStart');
-
-      const metrics = metricsRef.current;
-      metrics.isDragging = true;
-      metrics.startY = e instanceof MouseEvent ? e.pageY : e.touches[0].pageY;
-      if (sheetRef.current) {
-        metrics.startHeight = parseInt(sheetRef.current!.style.height, 10);
-        sheetRef.current.style.transition = 'none';
-      }
-    };
-
-    const dragging = (e: MouseEvent | TouchEvent) => {
-      const metrics = metricsRef.current;
-      if (!metrics.isDragging) return;
-
-      const delta =
-        e instanceof MouseEvent
-          ? metrics.startY - e.pageY
-          : metrics.startY - e.touches[0].pageY;
-
-      const newHeight =
-        metrics.startHeight + (delta / window.innerHeight) * 100;
-
-      console.log('dragging', `${metrics.startHeight} -> ${newHeight}`);
-      updateSheetHeight(newHeight);
-    };
-
-    const dragStop = () => {
-      const metrics = metricsRef.current;
-      if (!metrics.isDragging) return;
-      metrics.isDragging = false;
-
-      console.log('dragStop');
-      if (sheetRef.current) {
-        sheetRef.current.style.transition = 'height 0.3s ease';
-      }
-      const sheetHeight = parseInt(sheetRef.current!.style.height, 10);
-      if (sheetHeight < 25) onClose();
-      else if (sheetHeight > 75) updateSheetHeight(100);
-      else updateSheetHeight(50);
-    };
-
-    const updateSheetHeight = (height: number) => {
-      if (sheetRef.current) sheetRef.current.style.height = `${height}vh`;
-    };
-
-    updateSheetHeight(50);
-
-    sheetRef.current?.addEventListener('mousedown', dragStart);
-    document.addEventListener('mousemove', dragging);
-    document.addEventListener('mouseup', dragStop);
-
-    return () => {
-      sheetRef.current?.removeEventListener('mousedown', dragStart);
-      document.removeEventListener('mousemove', dragging);
-      document.removeEventListener('mouseup', dragStop);
-    };
-  }, []);
+  const { sheetRef } = useBottomSheet();
 
   return (
     <StyledBottomSheetWrapper
