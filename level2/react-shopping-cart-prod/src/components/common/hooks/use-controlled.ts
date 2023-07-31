afea1445f2ad709172df22b80ea9b33ccbc4c1c4
김민수 MINSOO KIM <alstn113@gmail.com>
2023-07-31 22:29:47 +0900

    feat: use controlled hook

---
+++ level2/react-shopping-cart-prod/src/components/common/hooks/use-controlled.ts
@@ -0,0 +1,25 @@
+import React, { useState, useCallback, useRef } from 'react';
+
+interface UseControlledProps<T> {
+  controlledValue?: T;
+  defaultValue: T;
+}
+
+const useControlled = <T>({
+  controlledValue,
+  defaultValue,
+}: UseControlledProps<T>) => {
+  const [state, setState] = useState(defaultValue);
+
+  const { current: isControlled } = useRef(controlledValue !== undefined);
+
+  const value = isControlled ? controlledValue : state;
+  const setValue: React.Dispatch<React.SetStateAction<T>> = useCallback(
+    (newState) => !isControlled && setState(newState),
+    [isControlled],
+  );
+
+  return [value, setValue];
+};
+
+export default useControlled;
