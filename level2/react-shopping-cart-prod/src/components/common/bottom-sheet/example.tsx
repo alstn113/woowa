eb279fe6730b457d618c04362e0830c635774d29
김민수 MINSOO KIM <alstn113@gmail.com>
2023-08-01 19:09:21 +0900

    feat: bottom sheet example

---
+++ level2/react-shopping-cart-prod/src/components/common/bottom-sheet/example.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+
+import BottomSheet from './index';
+
+const BottomSheetExample = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen((prev) => !prev);
+  return (
+    <div>
+      <button type="button" onClick={toggle}>
+        toggle
+      </button>
+      <BottomSheet isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <BottomSheet.Header />
+        <BottomSheet.Content>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
+            eligendi voluptates totam repellat sint, consequatur ipsa corporis
+            labore cupiditate beatae sed non architecto, aliquam similique
+            corrupti officiis sit voluptate dolorum.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
+            eligendi voluptates totam repellat sint, consequatur ipsa corporis
+            labore cupiditate beatae sed non architecto, aliquam similique
+            corrupti officiis sit voluptate dolorum.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
+            eligendi voluptates totam repellat sint, consequatur ipsa corporis
+            labore cupiditate beatae sed non architecto, aliquam similique
+            corrupti officiis sit voluptate dolorum.
+          </p>
+        </BottomSheet.Content>
+      </BottomSheet>
+    </div>
+  );
+};
+
+export default BottomSheetExample;
--- level2/react-shopping-cart-prod/src/pages/NotFoundPage.tsx
+++ level2/react-shopping-cart-prod/src/pages/NotFoundPage.tsx
@@ -1,41 +1,5 @@
-import { useState } from 'react';
-
-import BottomSheet from '../components/common/bottom-sheet';
-
 const NotFoundPage = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggle = () => setIsOpen((prev) => !prev);
-  return (
-    <div>
-      <button type="button" onClick={toggle}>
-        toggle
-      </button>
-      <BottomSheet isOpen={isOpen} onClose={() => setIsOpen(false)}>
-        <BottomSheet.Header />
-        <BottomSheet.Content>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
-            eligendi voluptates totam repellat sint, consequatur ipsa corporis
-            labore cupiditate beatae sed non architecto, aliquam similique
-            corrupti officiis sit voluptate dolorum.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
-            eligendi voluptates totam repellat sint, consequatur ipsa corporis
-            labore cupiditate beatae sed non architecto, aliquam similique
-            corrupti officiis sit voluptate dolorum.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
-            eligendi voluptates totam repellat sint, consequatur ipsa corporis
-            labore cupiditate beatae sed non architecto, aliquam similique
-            corrupti officiis sit voluptate dolorum.
-          </p>
-        </BottomSheet.Content>
-      </BottomSheet>
-    </div>
-  );
+  return <div>NotFoundPage</div>;
 };
 
 export default NotFoundPage;
