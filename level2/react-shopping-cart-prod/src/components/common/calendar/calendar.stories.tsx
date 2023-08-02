73258869c9a35bf4ca18b5601475ea19c28d1323
김민수 MINSOO KIM <alstn113@gmail.com>
2023-08-02 19:59:22 +0900

    feat: controlled, uncontrolled storybook

--- level2/react-shopping-cart-prod/src/components/common/calendar/calendar.stories.tsx
+++ level2/react-shopping-cart-prod/src/components/common/calendar/calendar.stories.tsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+
+import { format } from 'date-fns';
 import type { Meta, StoryObj } from '@storybook/react';
 
 import Calendar from './calendar';
@@ -12,4 +15,31 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const ControlledCalendar: Story = {
+  render: () => {
+    const [calendarState, setCalendarState] = useState(new Date());
+
+    return (
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          gap: '16px',
+        }}
+      >
+        <div>Selected Date: {format(calendarState, 'yyyy-MM-dd')}</div>
+        <Calendar
+          value={calendarState}
+          onChange={(date) => setCalendarState(date)}
+        />
+      </div>
+    );
+  },
+};
+
+export const UncontrolledCalendar: Story = {
+  render: () => {
+    return <Calendar />;
+  },
+};
