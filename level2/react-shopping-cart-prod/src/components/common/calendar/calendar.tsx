15fd568ee8a81d7533900c0be8d7f88133d03e4c
김민수 MINSOO KIM <alstn113@gmail.com>
2023-08-02 18:05:13 +0900

    feat: getWeekArray utils ㄹㅑ

--- level2/react-shopping-cart-prod/src/components/common/calendar/calendar.tsx
+++ level2/react-shopping-cart-prod/src/components/common/calendar/calendar.tsx
@@ -1,15 +1,9 @@
 import { useState } from 'react';
 
-import {
-  addDays,
-  endOfMonth,
-  endOfWeek,
-  isBefore,
-  startOfMonth,
-  startOfWeek,
-} from 'date-fns';
+import { startOfMonth } from 'date-fns';
 import styled from '@emotion/styled';
 
+import { getWeekArray } from './utils/getWeekArray';
 import CalendarWeekdayLabel from './calendar-weekday-label';
 import CalendarHeader from './calendar-header';
 import CalendarDay from './calendar-day';
@@ -17,26 +11,6 @@ import CalendarDay from './calendar-day';
 const Calendar = () => {
   const now = new Date();
   const [currentMonth, setCurrentMonth] = useState<Date>(startOfMonth(now));
-
-  const getWeekArray = (value: Date) => {
-    const start = startOfWeek(startOfMonth(value));
-    const end = endOfWeek(endOfMonth(value));
-
-    let count = 0;
-    let current = start;
-    const nestedWeeks: Date[][] = [];
-
-    while (isBefore(current, end)) {
-      const weekNumber = Math.floor(count / 7);
-      nestedWeeks[weekNumber] = nestedWeeks[weekNumber] || [];
-      nestedWeeks[weekNumber].push(current);
-
-      current = addDays(current, 1);
-      count += 1;
-    }
-
-    return nestedWeeks;
-  };
 
   const weeksToDisplay = getWeekArray(currentMonth);
 
