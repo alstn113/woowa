04154135ab2831e081b4a5b6d80030c7667959f0
김민수 MINSOO KIM <alstn113@gmail.com>
2023-08-17 15:41:04 +0900

    refactor: use portal dependecies

--- level2/react-shopping-cart-prod/src/components/common/hooks/use-portal.ts
+++ level2/react-shopping-cart-prod/src/components/common/hooks/use-portal.ts
@@ -32,7 +32,9 @@ const usePortal = (selectId: string): HTMLElement | null => {
     return () => {
       parentElement.removeChild(el);
     };
-  }, [id]);
+    // Id가 변경될 일이 없으므로 빈 배열을 넣어준다.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return elSnapshot;
 };
