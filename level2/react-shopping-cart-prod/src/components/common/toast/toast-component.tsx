ad270dd5e36b6c970af8b88294b04fb8c0a48b6e
김민수 MINSOO KIM <alstn113@gmail.com>
2023-08-20 19:34:34 +0900

    feat: toast component timeout

--- level2/react-shopping-cart-prod/src/components/common/toast/toast-component.tsx
+++ level2/react-shopping-cart-prod/src/components/common/toast/toast-component.tsx
@@ -1,7 +1,62 @@
 // Framer motion 코드가 들어갈 부분.
 
-const ToastComponent = () => {
-  return <div>ToastComponent</div>;
+import { useCallback, useEffect, useRef, useState } from 'react';
+
+import { motion, Variants } from 'framer-motion';
+
+import useDidUpdateEffect from '../hooks/use-did-update-effect';
+
+interface ToastComponentProps {
+  duration: number;
+}
+
+const motionVariants: Variants = {
+  initial: {},
+  animate: {},
+  exit: {},
+};
+
+const ToastComponent = (props: ToastComponentProps) => {
+  const { duration } = props;
+  const [delay, setDelay] = useState<number | null>(duration);
+
+  useDidUpdateEffect(() => {
+    setDelay(duration);
+  }, [duration]);
+
+  const onMouseEnter = () => setDelay(null);
+  const onMouseLeave = () => setDelay(duration);
+
+  const close = useCallback(() => {}, []);
+
+  const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+
+  useEffect(() => {
+    if (!delay) return;
+
+    timeoutRef.current = setTimeout(() => {
+      close();
+    }, delay);
+
+    return () => {
+      if (!timeoutRef.current) return;
+      clearTimeout(timeoutRef.current);
+    };
+  }, [delay, close]);
+
+  return (
+    <motion.div
+      layout
+      variants={motionVariants}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      onHoverStart={onMouseEnter}
+      onHoverEnd={onMouseLeave}
+    >
+      ToastComponent
+    </motion.div>
+  );
 };
 
 export default ToastComponent;
