686e5cafdb56cd36bf20dea722fd1d213c881e1a
김민수 MINSOO KIM <alstn113@gmail.com>
2023-08-20 13:31:36 +0900

    feat: toastList in toast provider

--- level2/react-shopping-cart-prod/src/components/common/toast/toast-provider.tsx
+++ level2/react-shopping-cart-prod/src/components/common/toast/toast-provider.tsx
@@ -1,12 +1,28 @@
 // Root Provider에 추가할 Provider
 // useToas를 통해서 사용하기 위함.
 
+import { AnimatePresence } from 'framer-motion';
+
+import Portal from '../portal';
 import useToastStore from './toast-store';
+import ToastComponent from './toast-component';
 
 const ToastProvider = () => {
   const { toasts } = useToastStore();
 
-  return <div>ToastProvider</div>;
+  const toastList = () => {
+    return (
+      <div>
+        <AnimatePresence initial={false}>
+          {toasts.map((toast) => (
+            <ToastComponent key={toast.id} />
+          ))}
+        </AnimatePresence>
+      </div>
+    );
+  };
+
+  return <Portal id="toast">{toastList()}</Portal>;
 };
 
 export default ToastProvider;
